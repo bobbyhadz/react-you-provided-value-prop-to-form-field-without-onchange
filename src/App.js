a01565c5d1a33d665926f2f68332f195ca4a2452
bobbyhadz <opmftw@gmail.com>
2024-04-06 12:19:07 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <input
+        type="text"
+        id="message"
+        defaultValue="Initial value"
+      />
+    </div>
+  );
+}
